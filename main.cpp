56e93fd965fcf8862f195ddc1ca61058e31f4a9a
SeongHyeon Jo <127729846+seonjo1@users.noreply.github.com>
2024-05-09 08:18:08 +0000

    Update main : main문 생성 및 완성

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Server.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        std::cout << "usage : ./ircserv <port> <password>\n";
+    else
+    {
+        try
+        {
+            Server server(argv[1], argv[2]);
+            server.run();
+            return (0);
+        }
+        catch(const std::exception& e)
+            std::cout << e.what() << std::endl;
+    }
+    return (1);
+}
