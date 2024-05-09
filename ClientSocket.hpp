6039df5d0d1daf5b75b877eb1a1b2a0ec06a31fb
SeongHyeon Jo <127729846+seonjo1@users.noreply.github.com>
2024-05-09 10:51:03 +0000

    Update Project : ClientSocket.hpp 헤더 기본 틀 생성

--- ClientSocket.hpp
+++ ClientSocket.hpp
@@ -1,21 +1,16 @@
-#ifndef ASOCKET_HPP
-# define ASOCKET_HPP
+#ifndef CLIENTSOCKET_HPP
+# define CLIENTSOCKET_HPP
 
-# include <sys/socket.h>
+# include "ASocket.hpp"
 
-class ASocket
+class ClientSocket : public ASocket
 {
 public:
-	ASocket();
-	~ASocket();
-	ASocket(const ASocket& copy);
-	ASocket& operator=(const ASocket& copy);
-	virtual void create() = 0;
-
-private:
-	int _socket;
-	struct sockaddr_in _addr;
-	int _addrLen;
+	ClientSocket();
+	~ClientSocket();
+	ClientSocket(const ClientSocket& copy);
+	ClientSocket& operator=(const ClientSocket& copy);
+	virtual void create();
 }
 
 #endif
