6582de9d379a561c1e49d65d10a191c8de3bd40d
seonjo1 <tjdgus9684@gmail.com>
2024-05-10 22:13:59 +0900

    Update Message : 주석 추가

--- Message.cpp
+++ Message.cpp
@@ -11,20 +11,27 @@ void Message::fillMsg()
 
 	while (1)
 	{
+		// 메시지 수신
 		readNum = recv(fd, &tmp, 1, 0);
 		if (readNum == -1)
 		{
-			if (errno != EAGAIN && errno != EWOULDBLOCK)
-				errFlag = true;
+			// errno 가 EAGAIN 이나 EWOULDBLOCK 인 경우 recv() 에러가 아닌
+			// 비동기 소켓이 읽을 내용이 없어서 block 되지 않고 -1 반환 (따라서 오류가 아님)
+			if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+
+			// 위 if 문에 걸리지 않는 경우는 recv() 에러
+			errFlag = true;
 			break;
 		}
 		else if (readNum == 0)
 		{
+			// 클라이언트와 연결 종료 (eof)
 			endFlag = true;
 			break;
 		}
-		else if (tmp == '\n')
+		else if (tmp == '\n') // 메시지 끝까지 읽었으니 종료
 			break;
+		// 버퍼에 메시지 추가
 		buf += tmp;
 	}
 }
@@ -32,23 +39,28 @@ void Message::fillMsg()
 
 bool Message::isError()
 {
+	// 에러 플래그 반환
 	return (errFlag);
 }
 
 bool Message::isDisconnected()
 {
+	// 연결이 끊겼는지 나타내는 플래그 반환
 	return (endFlag);
 }
 
 bool Message::isComplete()
 {
+	// 마지막이 '\r'으로 끝난 버퍼는 명령어가 완성됐다는 뜻!!
 	if (buf.size() != 0 && buf[buf.size() - 1] == '\r')
 		return (true);
+	// 그게 아니면 아직 명령어가 덜 도착했으므로 다시 대기
 	return (false);
 }
 
 std::string Message::getMsg()
 {
+	// 가지고 있던 메시지를 return 하면서 버퍼 초기화
 	std::string tmp(buf);
 	buf = "";
 	return (tmp);
--- Message.hpp
+++ Message.hpp
@@ -11,17 +11,17 @@ public:
 	Message(int fd);
 	~Message();
 
-	bool isError();
-	bool isDisconnected();
-	void fillMsg();
-	bool isComplete();
-	std::string getMsg();
+	std::string getMsg(); // 버퍼에 있는 내용 반환
+	void fillMsg(); // 클라이언트가 보낸 데이터 버퍼에 저장
+	bool isError(); // recv 함수 에러 발생 여부 확인
+	bool isDisconnected(); // 클라이언트와 연결 종료 여부 확인
+	bool isComplete(); // 버퍼에 있는 명령어 완성 확인
 
 private:
-	int fd;
-	std::string buf;
-	bool endFlag;
-	bool errFlag;
+	int fd; // 데이터를 받는 fd
+	std::string buf; // 메시지 저장 버퍼
+	bool endFlag; // 클라이언트와 연결 종료 플래그
+	bool errFlag; // recv 함수 에러 플래그
 };
 
 #endif
