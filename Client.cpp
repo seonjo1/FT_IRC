c38b03e7eb56e3e0237a89ab7f7375859ebc3611
seonjo1 <tjdgus9684@gmail.com>
2024-05-10 22:13:22 +0900

    Update Client : 주석 추가

--- Client.cpp
+++ Client.cpp
@@ -6,13 +6,16 @@ Client::~Client() {};
 
 void Client::fillMsg()
 {
+	// 메시지에 클라이언트가 보낸 내용을 저장
 	msg.fillMsg();
+	// 메시지를 저장하다 발생한 에러 감지
 	if (msg.isError())
 		throw std::runtime_error("recv() error");
 }
 
 bool Client::isDisconnected()
 {
+	// 연결이 끊겼는지 확인
 	if (msg.isDisconnected())
 		return (true);
 	return (false);
@@ -20,6 +23,7 @@ bool Client::isDisconnected()
 
 bool Client::isCmdComplete()
 {
+	// 메시지 버퍼에 있는 명령어가 완성됐는지 확인
 	if (msg.isComplete())
 		return (true);
 	return (false);
@@ -27,5 +31,6 @@ bool Client::isCmdComplete()
 
 std::string Client::getMsg()
 {
+	// 메시지 반환
 	return (msg.getMsg());
 }
--- Client.hpp
+++ Client.hpp
@@ -9,13 +9,13 @@ public:
 	Client(int fd);
 	~Client();
 
-	void fillMsg();
-	bool isDisconnected();
-	bool isCmdComplete();
-	std::string getMsg();
+	void fillMsg(); // 클라이언트가 보낸 데이터를 메시지에 채우는 함수
+	bool isDisconnected(); // 클라이언트와 연결 여부 확인
+	bool isCmdComplete(); // 버퍼에 있는 명령어 완성 여부 확인
+	std::string getMsg(); // 버퍼에 있는 메시지 반환
 
 private:
-	Message msg;
+	Message msg; // 클라이언트와 통신할때 사용할 객체
 };
 
 #endif
