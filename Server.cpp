b08f4f52e382b866e391f5b4d86e26299f691ab2
seonjo1 <tjdgus9684@gmail.com>
2024-05-10 17:07:13 +0900

    Update Server : kqueue 기능 추가 및 run 반복문 틀 완성

--- Server.cpp
+++ Server.cpp
@@ -4,14 +4,36 @@ Server::Server(char *port, char *password)
 	: password(password)
 {
 	Socket::makeServerSocket(port);
+	kq.socketAdd(Socket::servSocket);
 }
 
 Server::~Server() {};
+
+void Server::receiveClientRequest(int fd)
+{
+	if (fd == Socket::servSocket)
+	{
+		int clientSocket = Socket::makeClientSocket();
+		kq.socketAdd(clientSocket);
+		clientList.insert({clientSocket, Client()});
+	}
+	else
+	{
+		
+	}
+}
 
 void Server::run()
 {
 	while (1)
 	{
-		break;
+		int eventNum = kq.event();
+		for (int idx = 0; idx < eventNum; idx++)
+		{
+			if (kq.flagCheck(idx))
+				receiveClientRequest(kq.getFd(idx));
+			else
+				throw std::runtime_error("kevent() EV_ERROR flag");
+		}
 	}
 }
--- Server.hpp
+++ Server.hpp
@@ -1,7 +1,11 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include <map>
 # include "Socket.hpp"
+# include "Client.hpp"
+# include "Channel.hpp"
+# include "Kqueue.hpp"
 
 class Server
 {
@@ -9,7 +13,13 @@ public:
 	Server(char *port, char *password);
 	~Server();
 
+
+	Kqueue kq;
+	std::map<int, Client> clientList;
+	std::map<std::string, Channel> channelList;
+
 	void run();
+	void receiveClientRequest(int fd);
 
 private:
 	char *password;
