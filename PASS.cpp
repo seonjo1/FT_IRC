329f79f835db13b136f861cd5cd06f58364253e7
seonjo1 <tjdgus9684@gmail.com>
2024-05-16 19:55:19 +0900

    Update PASS : PASS 명령어 구현

--- PASS.cpp
+++ PASS.cpp
@@ -1,29 +1,28 @@
 #include "Executor.hpp"
+#include "Server.hpp"
 
 void Executor::PASS(Client& client, std::vector<std::string>& cmds)
 {
-	int fd = client.getFd();
 	// 이미 PASS 인증이 끝난 유저
 	if (client.getPassFlag())
 	{
-		sendMsg(fd, ErrMsg::ALREADYREGISTER(client.getNick()));
+		client.sendMsg(ErrMsg::ALREADYREGISTER(client.getNick()));
 		return ;
 	}
 	
 	// 인자 개수가 틀린 경우
 	if (cmds.size() != 2)
 	{
-		sendMsg(fd, ErrMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
+		client.sendMsg(ErrMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
 		return ;
 	}
 
 	// 비밀번호가 틀린 경우
 	if (cmds[1] != password)
 	{
-		sendMsg(fd, ErrMsg::PASSWDMISMATCH());
+		client.sendMsg(ErrMsg::PASSWDMISMATCH());
 		// 연결 종료
-		kq->removeSocket(fd);
-		clientList->erase(fd);
+		Server::clientList.erase(client.getFd());
 	}
 
 	client.setPassFlag(true);
