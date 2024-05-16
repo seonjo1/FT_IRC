95243a06bbb3bb579a051a3a4bd6d5cbe8ec0395
seonjo1 <tjdgus9684@gmail.com>
2024-05-16 22:22:22 +0900

    Update PASS : nick 있을때와 없을때 구분해서 메시지 처리

--- PASS.cpp
+++ PASS.cpp
@@ -6,21 +6,21 @@ void Executor::PASS(Client& client, std::vector<std::string>& cmds)
 	// 이미 PASS 인증이 끝난 유저
 	if (client.getPassFlag())
 	{
-		client.sendMsg(ErrMsg::ALREADYREGISTER(client.getNick()));
+		client.sendMsg(ServerMsg::ALREADYREGISTER(client.getNick()));
 		return ;
 	}
 	
 	// 인자 개수가 틀린 경우
 	if (cmds.size() != 2)
 	{
-		client.sendMsg(ErrMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
+		client.sendMsg(ServerMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
 		return ;
 	}
 
 	// 비밀번호가 틀린 경우
 	if (cmds[1] != password)
 	{
-		client.sendMsg(ErrMsg::PASSWDMISMATCH());
+		client.sendMsg(ServerMsg::PASSWDMISMATCH());
 		// 연결 종료
 		Server::clientList.erase(client.getFd());
 	}
