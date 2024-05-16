b1901857f3a3547f58cedb92126b1b58cbf941b7
seonjo1 <tjdgus9684@gmail.com>
2024-05-16 17:01:26 +0900

    Update Channel : git 인식 안되는거 해결

--- Channel.cpp
+++ Channel.cpp
@@ -1,12 +1,13 @@
+#include "Server.hpp"
 #include "Channel.hpp"
 
 Channel::Channel(std::string& channelName)
-    : channelName(channelName)
+	: channelName(channelName)
 {
-    timeChannelWasCreated = time();
+	timeChannelWasCreated = time(0);
 }
 
-Channel::~Channel();
+Channel::~Channel() {};
 
  // channel 이름 유효성 검사
 bool Channel::isInvalidChannelName(std::string& channel)
@@ -17,13 +18,13 @@ bool Channel::isInvalidChannelName(std::string& channel)
 // channel이 존재하는지 확인
 bool Channel::isChannelInUse(std::string& channel)
 {
-    std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
-    for (; iter != Server::channelList.end(); iter++)
-    {
-        if (iter->second.getName() == channel)
-            return (true);
-    }
-    return (false);
+	std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
+	for (; iter != Server::channelList.end(); iter++)
+	{
+		if (iter->second.getName() == channel)
+			return (true);
+	}
+	return (false);
 }
 
 // channel에 참가
@@ -44,60 +45,59 @@ void Channel::changeNickInChannel(std::string& oldNick, std::string& newNick)
 
 }
 
-
 // 채널에 메시지 전송
 void Channel::sendToClients(std::string& msg)
 {
-    std::vector<Client>::iterator iter = opList.begin();
-    for (; iter != opList.begin(); iter++)
-        iter->sendMsg(msg);
-    iter = joinList.begin();
-    for (; iter != joinList.begin(); iter++)
-        iter->sendMsg(msg);
+	std::vector<Client>::iterator iter = opList.begin();
+	for (; iter != opList.begin(); iter++)
+		iter->sendMsg(msg);
+	iter = joinList.begin();
+	for (; iter != joinList.begin(); iter++)
+		iter->sendMsg(msg);
 }
 
 // 채널에 클라이언트 있나 확인
 bool Channel::doesClientExist(std::string& nick)
 {
-    std::vector<Client>::iterator iter = opList.begin();
-    for (; iter != opList.begin(); iter++)
-    {
-        if (iter->nickname == nick)
-            return (true);
-    }
-    iter = joinList.begin();
-    for (; iter != joinList.begin(); iter++)
-    {
-        if (iter->nickname == nick)
-            return (true);
-    }
-    return (false);
+	std::vector<Client>::iterator iter = opList.begin();
+	for (; iter != opList.begin(); iter++)
+	{
+		if (iter->getNick() == nick)
+			return (true);
+	}
+	iter = joinList.begin();
+	for (; iter != joinList.begin(); iter++)
+	{
+		if (iter->getNick() == nick)
+			return (true);
+	}
+	return (false);
 }
 
 // 채널 삭제 (채널리스트에서 삭제)
 void Channel::removeChannel(std::string& channel)
 {
-    std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
-    for (; iter != Server::channelList.end(); iter++)
-    {
-        if (iter->second.getName() == channel)
-            Server::channelList.erase(iter);
-    }
+	std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
+	for (; iter != Server::channelList.end(); iter++)
+	{
+		if (iter->second.getName() == channel)
+			Server::channelList.erase(iter);
+	}
 }
 
 // 채널 리스트에서 채널 찾아주는 함수
 Channel& Channel::findChannel(std::string& channel)
 {
-    std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
-    for (; iter != Server::channelList.end(); iter++)
-    {
-        if (iter->second.getName() == channel)
-            return (iter->second);
-    }
+	std::map<std::string, Channel>::iterator iter = Server::channelList.begin();
+	for (; iter != Server::channelList.end(); iter++)
+	{
+		if (iter->second.getName() == channel)
+			return (iter->second);
+	}
 }
 
 // getter
 std::string Channel::getName()
 {
-    return (channelName);
+	return (channelName);
 }
--- Channel.hpp
+++ Channel.hpp
@@ -7,47 +7,49 @@
 class Channel
 {
 public:
-    Channel(std::string& channelName);
-    ~Channel();
+	Channel(std::string& channelName);
+	~Channel();
+
+	// 채널 관리 (참가자 명단, operator 명단, 초대 명단)
+	bool isInvalidChannelName(std::string& channel); // channel 이름 유효성 검사
+	bool isChannelInUse(std::string& channel); // channel 이 존재하는지
+	void addNickInChannel(std::string& nick);
+	void removeNickInChannel(std::string& nick);
+	void changeNickInChannel(std::string& oldNick, std::string& newNick);
+
+	// getter
+	std::string getName();
 
 private:
 // 변수
-    std::string channelName; // 채널 이름
-    int timeChannelWasCreated// 채널 생성 시간
+	std::string channelName; // 채널 이름
+	int timeChannelWasCreated;// 채널 생성 시간
 
-    std::vector<Client> joinList// 참가자 명단
-    std::vector<Client> opList// operator 명단
-    std::vector<std::string> inviteList// 초대명단
+	std::vector<Client> joinList;// 참가자 명단
+	std::vector<Client> opList;// operator 명단
+	std::vector<std::string> inviteList;// 초대명단
 
-    std::string mode;// mode
+	std::string mode;// mode
 
-    std::string topic;// topic
-    std::string topicWriter// topic 작성자
-    int timeTopicWasWritten// topic 생성시간
-    
-    std::string key;// 채널 key
+	std::string topic;// topic
+	std::string topicWriter;// topic 작성자
+	int timeTopicWasWritten;// topic 생성시간
+		
+	std::string key;// 채널 key
 
-    int limit;// limit
+	int limit;// limit
 
 //함수
-    // 채널 관리 (참가자 명단, operator 명단, 초대 명단)
-        bool isInvalidChannelName(std::string& channel); // channel 이름 유효성 검사
-    	bool isChannelInUse(std::string& channel); // channel 이 존재하는지
-        void addNickInChannel(std::string& nick);
-        void removeNickInChannel(std::string& nick);
-        void changeNickInChannel(std::string& oldNick, std::string& newNick);
 
-    // 채널에 메시지 전송
-        void sendToClients(std::string& msg);
-    // 채널에 클라이언트 있나 확인
-        bool doesClientExist(std::string& nick);
-    // 채널 삭제 (채널리스트에서 삭제)
-        void removeChannel(std::string& channel);
-    // 채널 리스트에서 채널 찾아주는 함수
-        Channel& findChannel(std::string& channel);
+	// 채널에 메시지 전송
+		void sendToClients(std::string& msg);
+	// 채널에 클라이언트 있나 확인
+		bool doesClientExist(std::string& nick);
+	// 채널 삭제 (채널리스트에서 삭제)
+		void removeChannel(std::string& channel);
+	// 채널 리스트에서 채널 찾아주는 함수
+		Channel& findChannel(std::string& channel);
 
-    // getter
-    std::string getName();
-}
+};
 
 #endif
