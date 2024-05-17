f7a5883bd8c48e4ec9b2cb6a753631b071f3e903
seonjo1 <tjdgus9684@gmail.com>
2024-05-17 20:54:09 +0900

    Update USER : parseUSER 추가

--- USER.cpp
+++ USER.cpp
@@ -1,6 +1,32 @@
 #include "Executor.hpp"
 #include "Server.hpp"
 
+void Executor::parseUSER(std::vector<std::string>& cmds, std::string& msg)
+{
+	int size = msg.size();
+	int argNum = 0;
+	int i = 0;
+	while (1)
+	{
+		if (argNum == 4 || i == size) break;
+		while (i < size && msg[i] == ' ')
+			i++;
+		std::string cmd;
+		while (i < size && msg[i] != ' ')
+			cmd += msg[i++];
+		if (cmd.size() > 0)
+		{
+			cmds.push_back(cmd);
+			argNum++;
+		}
+	}
+
+	while (i < size && msg[i] != ':')
+		i++;
+	if (++i != size)
+		cmds.push_back(msg.substr(i));
+}
+
 void Executor::USER(Client& client, std::vector<std::string>& cmds)
 {
 	// PASS 등록 안된 클라이언트
@@ -8,11 +34,12 @@ void Executor::USER(Client& client, std::vector<std::string>& cmds)
 	{
 		client.sendMsg(ServerMsg::NOTREGISTERD(client.getNick()));
 		// 연결 종료
-		Server::clientList().erase(client.getFd());
+		std::map<int, Client>& clientList = Server::getClientList();
+		clientList.erase(client.getFd());
 	}
 
 	// 인자 개수가 다른 경우
-	if (cmds.size() != 5)
+	if (cmds.size() < 5)
 	{
 		client.sendMsg(ServerMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
 		return ;
