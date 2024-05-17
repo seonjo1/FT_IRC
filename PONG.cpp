8fdec410c9a148f06fab2fd3b04fb19e92dc53b8
seonjo1 <tjdgus9684@gmail.com>
2024-05-17 15:42:09 +0900

    Update PONG : PONG 명령어 완성

--- PONG.cpp
+++ PONG.cpp
@@ -3,5 +3,17 @@
 
 void Executor::PONG(Client& client, std::vector<std::string>& cmds)
 {
-	
+	if (cmds.size() < 3)
+		client.sendMsg(ServerMsg::NOORIGIN(client.getNick()));
+	else if (cmds[1] != Server::IP)
+		client.sendMsg(ServerMsg::NOSUCHSERVER(client.getNick(), cmds[1]));
+	else if (cmds[2] != (":" + Server::IP))
+		client.sendMsg(ServerMsg::NOSUCHSERVER(client.getNick(), cmds[2].substr(1)));
+	else
+	{
+		client.sendMsg(ServerMsg::WELCOME(client.getNick(), client.getHostName(), client.getServerName(), ));
+		client.sendMsg(ServerMsg::NOMOTD(client.getNick()));
+		return ;
+	}
+	Server::clientList.erase(client.getFd());
 }
