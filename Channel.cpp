9c115279e360fa888ced3dada37f806acde122c8
seonjo1 <tjdgus9684@gmail.com>
2024-05-18 22:22:14 +0900

    Update Channel : JOIN을 위한 함수들 추가

--- Channel.cpp
+++ Channel.cpp
@@ -2,9 +2,71 @@
 #include "Channel.hpp"
 
 Channel::Channel(std::string& channelName)
-	: channelName(channelName), timeChannelWasCreated(time(0)) {};
+	: channelName(channelName), timeChannelWasCreated(time(0)), topicFlag(false) {};
 
 Channel::~Channel() {};
+
+// 채널에 참가
+void Channel::joinChannel(Client& client, std::string& channelName, std::string param)
+{
+	std::vector<std::string> room;
+	// 할일 /join #room1, #room2, key1, key2 일때 pair로 파싱하기
+
+	if (isChannelInUse(channelName)) // 기존 채널이 있는 경우
+	{
+		// channel 찾기
+		Channel& channel = findChannel(channelName);
+		
+		// key 모드인데 key가 틀린경우
+		if (channel.isKeyMode() && channel.getKey() != param)
+		{
+			client.sendMsg(ServerMsg::BADCHANNELKEY(client.getNick(), channel.getName()));
+			return ;
+		}
+
+		// invite 여부 확인
+		if (channel.isInviteMode() && channel.isInvitedClient(client.getNick()))
+		{
+			client.sendMsg(ServerMsg::INVITEONLYCHAN(client.getNick(), channel.getName()));
+			return ;
+		}
+
+		// 풀방 확인
+		if (channel.isLimitMode() && channel.getLimit() <= channel.getSize())
+		{
+			client.sendMsg(ServerMsg::CHANNELISFULL(client.getNick(), channel.getName()));
+			return ;
+		}
+
+		// 채널에 참여
+		channel.addNickInChannel(client);
+
+		// 토픽확인하고 참가자에게 토픽 메시지 전송
+		if (channel.doesTopicExist())
+		{
+			client.sendMsg(ServerMsg::TOPIC(client.getNick(), channel.getName(), channel.getTopic()));
+			client.sendMsg(ServerMsg::TOPICINFO(client.getNick(), channel.getName(),
+												channel.getTopicWriter(), channel.getTopicTime()));
+		}
+
+		// 채널에 모든이들에게 join 메시지전송
+		channel.sendToClients(ServerMsg::JOIN(client.getNick(), client.getHostName(),
+											client.getServerName(), channel.getName()));
+	}
+	else // 채널이 없어서 만들어야하는 경우
+	{
+		Channel& channel = addChannel(channelName);
+
+		// 채널에 참여
+		channel.addNickInChannel(client);
+
+		// join 메시지전송
+		client.sendMsg(ServerMsg::JOIN(client.getNick(), client.getHostName(),
+									 client.getServerName(), channel.getName()));
+	
+	}
+}
+
 
  // channel 이름 유효성 검사
 bool Channel::isInvalidChannelName(std::string& channel)
@@ -43,22 +105,24 @@ bool Channel::isChannelInUse(std::string& channel)
 void Channel::addNickInChannel(Client& client)
 {
 	if (joinList.size() == 0 && opList.size() == 0)
-		opList.push_back(client);
+		opList.push_back(&client);
 	else
-		joinList.push_back(client);
+		joinList.push_back(&client);
 	std::vector<std::string>::iterator iter = find(inviteList.begin(), inviteList.end(), client.getNick());
 	if (iter != inviteList.end())
 		inviteList.erase(iter);
+	
+	client.addJoinedChannels(this);
 }
 
 // channel 나가기
 void Channel::removeNickInChannel(Client& client)
 {
-	std::vector<Client>::iterator iter = find(joinList.begin(), joinList.end(), client);
+	std::vector<Client*>::iterator iter = find(joinList.begin(), joinList.end(), &client);
 	if (iter != joinList.end())
 		joinList.erase(iter);
 	
-	iter = find(opList.begin(), opList.end(), client);
+	iter = find(opList.begin(), opList.end(),&client);
 	if (iter != opList.end())
 		opList.erase(iter);
 }
@@ -67,20 +131,20 @@ void Channel::removeNickInChannel(Client& client)
 void Channel::changeNickInChannel(Client& client, std::string& newNick, std::set<int>& set)
 {
 	// joinList에 있는 nick 변경
-	std::vector<Client>::iterator iter = find(joinList.begin(), joinList.end(), client);
+	std::vector<Client*>::iterator iter = find(joinList.begin(), joinList.end(), &client);
 	if (iter != joinList.end())
-		iter->setNick(newNick);
+		(*iter)->setNick(newNick);
 	
 	// opList에 있는 nick 변경
-	iter = find(opList.begin(), opList.end(), client);
+	iter = find(opList.begin(), opList.end(), &client);
 	if (iter != opList.end())
-		iter->setNick(newNick);
+		(*iter)->setNick(newNick);
 
 	// joinList와 opList에 있는 클라이언트들의 fd 수집
 	for (iter = joinList.begin(); iter != joinList.end(); iter++)
-		set.insert(iter->getFd());
+		set.insert((*iter)->getFd());
 	for (iter = opList.begin(); iter != opList.end(); iter++)
-		set.insert(iter->getFd());
+		set.insert((*iter)->getFd());
 
 	// inviteList에 있는 nick 변경
 	std::vector<std::string>::iterator inviteIter = find(inviteList.begin(), inviteList.end(), client.getNick());
@@ -91,31 +155,43 @@ void Channel::changeNickInChannel(Client& client, std::string& newNick, std::set
 // 채널에 메시지 전송
 void Channel::sendToClients(std::string msg)
 {
-	std::vector<Client>::iterator iter = opList.begin();
-	for (; iter != opList.begin(); iter++)
-		iter->sendMsg(msg);
+	std::vector<Client*>::iterator iter = opList.begin();
+	for (; iter != opList.end(); iter++)
+	{
+		std::cout << "member : " << (*iter)->getNick() << "\n";
+		(*iter)->sendMsg(msg);
+	}
 	iter = joinList.begin();
-	for (; iter != joinList.begin(); iter++)
-		iter->sendMsg(msg);
+	for (; iter != joinList.end(); iter++)
+		(*iter)->sendMsg(msg);
 }
 
 // 채널에 클라이언트 있나 확인
 bool Channel::doesClientExist(std::string& nick)
 {
-	std::vector<Client>::iterator iter = opList.begin();
+	std::vector<Client*>::iterator iter = opList.begin();
 	for (; iter != opList.begin(); iter++)
 	{
-		if (iter->getNick() == nick)
+		if ((*iter)->getNick() == nick)
 			return (true);
 	}
 	iter = joinList.begin();
 	for (; iter != joinList.begin(); iter++)
 	{
-		if (iter->getNick() == nick)
+		if ((*iter)->getNick() == nick)
 			return (true);
 	}
 	return (false);
 }
+
+// 채널 추가
+Channel& Channel::addChannel(std::string& channel)
+{
+	std::map<std::string, Channel>& channelList = Server::getChannelList();
+	channelList.insert(std::make_pair(channel, Channel(channel)));
+	return (findChannel(channel));
+}
+
 
 // 채널 삭제 (채널리스트에서 삭제)
 void Channel::removeChannel(std::string& channel)
@@ -154,8 +230,105 @@ Channel& Channel::findChannel(std::string& channel)
 	return (iter->second);
 }
 
+// mode 함수
+bool Channel::isKeyMode()
+{
+	if (mode.find("k") != std::string::npos)
+		return (true);
+	return (false);
+}
+
+bool Channel::isInviteMode()
+{
+	if (mode.find("i") != std::string::npos)
+		return (true);
+	return (false);
+
+}
+
+bool Channel::isLimitMode()
+{
+	if (mode.find("l") != std::string::npos)
+		return (true);
+	return (false);
+}
+
+bool Channel::doesTopicExist()
+{
+	return (topicFlag);
+}
+
+bool Channel::isInvitedClient(std::string nick)
+{
+	if (find(inviteList.begin(), inviteList.end(), nick) == inviteList.end())
+		return (false);
+	return (true);
+}
+
+
+void Channel::removeClientFromInvitedList(std::string nick)
+{
+	inviteList.erase(find(inviteList.begin(), inviteList.end(), nick));
+}
+
+
 // getter
 std::string Channel::getName()
 {
 	return (channelName);
+}
+
+std::string Channel::getKey()
+{
+	return (key);
+}
+
+int Channel::getLimit()
+{
+	return (limit);
+}
+
+int Channel::getSize()
+{
+	return (joinList.size() + opList.size());
+}
+
+std::string Channel::getTopic()
+{
+	return (topic);
+}
+
+std::string Channel::getTopicWriter()
+{
+	return (topicWriter);
+}
+
+int	Channel::getTopicTime()
+{
+	return (timeTopicWasWritten);
+}
+
+std::vector<Client*>& Channel::getJoinList()
+{
+	return (joinList);
+}
+
+std::vector<Client*>& Channel::getOpList()
+{
+	return (opList);
+}
+
+std::vector<std::string>& Channel::getInviteList()
+{
+	return (inviteList);
+}
+
+
+// setter
+void Channel::setTopic(Client& client, std::string topic)
+{
+	topicFlag = true;
+	this->topic = topic;
+	topicWriter = client.getNick();
+	timeTopicWasWritten = time(0);
 }
--- Channel.hpp
+++ Channel.hpp
@@ -11,26 +11,49 @@ public:
 	~Channel();
 
 	// 채널 관리 (참가자 명단, operator 명단, 초대 명단)
+	static void joinChannel(Client& client, std::string& channelName, std::string param); // 채널에 참가
 	static bool isInvalidChannelName(std::string& channel); // channel 이름 유효성 검사
 	static bool isChannelInUse(std::string& channel); // channel 이 존재하는지
 	void addNickInChannel(Client& client);
 	void removeNickInChannel(Client& client);
 	void changeNickInChannel(Client& client, std::string& newNick, std::set<int>& set);
 
+	// mode 함수
+	bool isKeyMode();
+	bool isInviteMode();
+	bool isLimitMode();
+	bool doesTopicExist();
+	bool isInvitedClient(std::string nick);
+	void removeClientFromInvitedList(std::string nick);
+
 	// getter
 	std::string getName();
+	std::string getKey();
+	int getLimit();
+	int getSize();
+	std::string getTopic();
+	std::string getTopicWriter();
+	int	getTopicTime();
+	std::vector<Client*>& getJoinList();
+	std::vector<Client*>& getOpList();
+	std::vector<std::string>& getInviteList();
+
+
+	// setter
+	void setTopic(Client& client, std::string topic);
 
 private:
 // 변수
 	std::string channelName; // 채널 이름
 	int timeChannelWasCreated;// 채널 생성 시간
 
-	std::vector<Client> joinList;// 참가자 명단
-	std::vector<Client> opList;// operator 명단
+	std::vector<Client*> joinList;// 참가자 명단
+	std::vector<Client*> opList;// operator 명단
 	std::vector<std::string> inviteList;// 초대명단
 
 	std::string mode;// mode
 
+	bool topicFlag;
 	std::string topic;// topic
 	std::string topicWriter;// topic 작성자
 	int timeTopicWasWritten;// topic 생성시간
@@ -44,10 +67,12 @@ private:
 		void sendToClients(std::string msg);
 	// 채널에 클라이언트 있나 확인
 		bool doesClientExist(std::string& nick);
+	// 채널 추가
+		static Channel& addChannel(std::string& channel);
 	// 채널 삭제 (채널리스트에서 삭제)
-		void removeChannel(std::string& channel);
+		static void removeChannel(std::string& channel);
 	// 채널 리스트에서 채널 찾아주는 함수
-		Channel& findChannel(std::string& channel);
+		static Channel& findChannel(std::string& channel);
 
 	// 모드 셋팅
 		void setMode(std::string& cmd);
