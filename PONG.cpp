b7fbfec3681fd60e4b1e5b62a52cb68b97ce4355
seonjo1 <tjdgus9684@gmail.com>
2024-05-19 14:39:23 +0900

    Update PONG : 파싱 마지막 인자가 ':'으로 시작 안할때 오류처리

--- PONG.cpp
+++ PONG.cpp
@@ -6,6 +6,8 @@ void Executor::parsePONG(std::vector<std::string>& cmds, std::string& msg)
 	int size = msg.size();
 	int argNum = 0;
 	int i = 0;
+
+	// 공백 기준으로 앞에 인자 2개 구분
 	while (1)
 	{
 		if (argNum == 2 || i == size) break;
@@ -21,8 +23,20 @@ void Executor::parsePONG(std::vector<std::string>& cmds, std::string& msg)
 		}
 	}
 
+	// 3번째 인자는 ':'으로 시작하면 인정
 	while(i < size && msg[i] != ':')
+	{
+		// 3번째 인자가 ':'으로 시작안하는 애면 오류
+		if (msg[i] != ' ')
+		{
+			cmds.push_back("error");
+			cmds.push_back("error");
+			return ;
+		}
 		i++;
+	}
+
+	// 3번째 인자 추가
 	if (++i < size)
 		cmds.push_back(msg.substr(i));
 }
@@ -33,7 +47,7 @@ void Executor::PONG(Client& client, std::vector<std::string>& cmds)
 		client.sendMsg(ServerMsg::NOORIGIN(client.getNick()));
 	else if (cmds[1] != Server::getIP())
 		client.sendMsg(ServerMsg::NOSUCHSERVER(client.getNick(), cmds[1]));
-	else
+	else if (cmds.size() < 4)
 	{
 		client.sendMsg(ServerMsg::WELCOME(client.getNick(), client.getHostName(), client.getServerName()));
 		client.sendMsg(ServerMsg::NOMOTD(client.getNick()));
