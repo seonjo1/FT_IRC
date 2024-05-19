a301f7d4ec14c663a95391ae616b2831d1f55068
seonjo1 <tjdgus9684@gmail.com>
2024-05-19 14:39:02 +0900

    Update PING : parsing 변경

--- PING.cpp
+++ PING.cpp
@@ -3,10 +3,42 @@
 
 void Executor::parsePING(std::vector<std::string>& cmds, std::string& msg)
 {
-	std::stringstream ss(msg);
-	std::string cmd;
-	while (ss >> cmd)
-		cmds.push_back(cmd);
+	int size = msg.size();
+	int argNum = 0;
+	int i = 0;
+
+	// 공백 기준으로 앞에 인자 2개 구분
+	while (1)
+	{
+		if (argNum == 2 || i == size) break;
+		while (i < size && msg[i] == ' ')
+			i++;
+		std::string cmd;
+		while (i < size && msg[i] != ' ')
+			cmd += msg[i++];
+		if (cmd.size() > 0)
+		{
+			cmds.push_back(cmd);
+			argNum++;
+		}
+	}
+
+	// 3번째 인자는 ':'으로 시작하면 인정
+	while(i < size && msg[i] != ':')
+	{
+		// 3번째 인자가 ':'으로 시작안하는 애면 오류
+		if (msg[i] != ' ')
+		{
+			cmds.push_back("error");
+			cmds.push_back("error");
+			return ;
+		}
+		i++;
+	}
+
+	// 3번째 인자 추가
+	if (++i < size)
+		cmds.push_back(msg.substr(i));
 }	
 
 void Executor::PING(Client& client, std::vector<std::string>& cmds)
@@ -19,7 +51,7 @@ void Executor::PING(Client& client, std::vector<std::string>& cmds)
 	{
 		client.sendMsg(ServerMsg::PING(cmds[1], ""));
 	}
-	else if (cmds.size() == 2)
+	else if (cmds.size() == 3)
 	{
 		client.sendMsg(ServerMsg::PING(cmds[1], cmds[2]));
 	}
