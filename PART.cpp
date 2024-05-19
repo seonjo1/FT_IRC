a536454e9ba35c52066223a3e456b8c0d6df8f59
seonjo1 <tjdgus9684@gmail.com>
2024-05-19 15:46:36 +0900

    Update PART : PART 구현 완료

--- PART.cpp
+++ PART.cpp
@@ -3,40 +3,95 @@
 
 void Executor::parsePART(std::vector<std::string>& cmds, std::string& msg)
 {
+	int size = msg.size();
+	int argNum = 0;
+	int i = 0;
 
+	// 공백 기준으로 앞에 인자 2개 구분
+	while (1)
+	{
+		if (argNum == 2 || i == size) break;
+		while (i < size && msg[i] == ' ')
+			i++;
+		std::string cmd;
+		while (i < size && msg[i] != ' ')
+			cmd += msg[i++];
+		if (cmd.size() > 0)
+		{
+			cmds.push_back(cmd);
+			argNum++;
+		}
+	}
+
+	// 3번째 인자는 ':'으로 시작하면 인정
+	while(i < size && msg[i] != ':')
+	{
+		// 3번째 인자가 ':'으로 시작안하는 애면 오류
+		if (msg[i] != ' ')
+		{
+			cmds.push_back("error");
+			cmds.push_back("error");
+			return ;
+		}
+		i++;
+	}
+
+	// 3번째 인자 추가
+	if (++i < size)
+		cmds.push_back(msg.substr(i));
 }
 
 void Executor::PART(Client& client, std::vector<std::string>& cmds)
 {
+	// 인자 부족
+	if (cmds.size() == 1 || cmds.size() > 3)
+	{
+		client.sendMsg(ServerMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
+		return ;
+	}
+	
+	// PART 명령어 파싱 2탄
+	// 1번째 인자의 ','를 ' '로 변경
+	for (int i = 0; i < static_cast<int>(cmds[1].size()); i++)
+	{
+		if (cmds[1][i] == ',')
+			cmds[1][i] = ' ';
+	}
+	// 첫 번째 인자를 ' '기준으로 나눔
+	std::vector<std::string>  roomVector;
+	std::stringstream ss(cmds[1]);
+	std::string str;
+	while (ss >> str)
+		roomVector.push_back(str);
 
+	for (int i = 0; i < static_cast<int>(roomVector.size()); i++)
+	{
+		std::string room = roomVector[i];
+
+		// 채널이 존재하지 않는경우
+		if (!Channel::isChannelInUse(room))
+		{
+			client.sendMsg(ServerMsg::NOSUCHCHANNEL(client.getNick(), room));
+			continue ;
+		}
+
+		// 내가 해당 채널에 없는 경우
+		Channel& channel = Channel::findChannel(room);
+		if (!channel.doesClientExist(client.getNick()))
+		{
+			client.sendMsg(ServerMsg::NOTONCHANNEL(client.getNick(), channel.getName()));
+			continue ;
+		}
+
+		if (cmds.size() == 2) // 메시지가 없는 경우
+		{
+			channel.sendToClients(ServerMsg::PART(client.getNick(), client.getHostName(), client.getServerName(), room, ""));
+		}
+		else // 메시지가 있는 경우
+		{
+			channel.sendToClients(ServerMsg::PART(client.getNick(), client.getHostName(), client.getServerName(), room, cmds[2]));
+		}
+		
+		channel.removeNickInChannel(client);
+	}
 }
-
-/*
-127.000.000.001.53282-127.000.000.001.06667: PART #c
-
-127.000.000.001.06667-127.000.000.001.53282: :nana_!codespace@127.0.0.1 PART :#c
-
-127.000.000.001.06667-127.000.000.001.53272: :nana_!codespace@127.0.0.1 PART :#c
-
-
-
-127.000.000.001.53282-127.000.000.001.06667: PART #c :a,b,c hi my
-
-127.000.000.001.06667-127.000.000.001.53282: :nana_!codespace@127.0.0.1 PART #c :a,b,c hi my
-
-127.000.000.001.06667-127.000.000.001.53272: :nana_!codespace@127.0.0.1 PART #c :a,b,c hi my
-
-127.000.000.001.53272-127.000.000.001.06667: PING irc.example.com
-
-127.000.000.001.06667-127.000.000.001.53272: :irc.example.com PONG irc.example.com :irc.example.com
-
-127.000.000.001.53282-127.000.000.001.06667: PING irc.example.com
-
-127.000.000.001.06667-127.000.000.001.53282: :irc.example.com PONG irc.example.com :irc.example.com
-
-127.000.000.001.53282-127.000.000.001.06667: PART #a,#b,#c :hi my
-
-127.000.000.001.06667-127.000.000.001.53282: :nana_!codespace@127.0.0.1 PART #a :hi my
-:nana_!codespace@127.0.0.1 PART #b :hi my
-:irc.example.com 442 nana_ #c :You're not on that channel
-*/
