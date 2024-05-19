1684f12e1140fadd05886f4992c46ee3dc639edb
seonjo1 <tjdgus9684@gmail.com>
2024-05-19 14:01:11 +0900

    Update JOIN : ',' 파싱 추가

--- JOIN.cpp
+++ JOIN.cpp
@@ -11,27 +11,52 @@ void Executor::parseJOIN(std::vector<std::string>& cmds, std::string& msg)
 
 void Executor::JOIN(Client& client, std::vector<std::string>& cmds)
 {
+
 	// 매개변수가 부족한 경우
 	if (cmds.size() == 1)
 	{
 		client.sendMsg(ServerMsg::NEEDMOREPARAMS(client.getNick(), cmds[0]));
 		return ;
 	}
-	// 잘못된 채널 이름인 경우
-	if (Channel::isInvalidChannelName(cmds[1]))
+
+	// join 명령어 파싱 2탄!
+	// 1번째 인자의 ','를 ' '로 변경
+	for (int i = 0; i < static_cast<int>(cmds[1].size()); i++)
 	{
-		client.sendMsg(ServerMsg::INVALIDCHANNEL(client.getNick(), cmds[1]));
-		return ;
+		if (cmds[1][i] == ',')
+			cmds[1][i] = ' ';
 	}
-	// 이미 참여한 채널인 경우
-	if (client.isClientMemberOfChannel(cmds[1]))
+	// 첫번째인자를 ' '기준으로 나누면서 나머지 인자들을 짝으로 맞춰줌
+	std::vector<std::pair<std::string, std::string> > cmdPairVector;
+	std::stringstream ss(cmds[1]);
+	int idx = 2;
+	std::string str;
+	while (ss >> str)
 	{
-		return ;
+		std::string pair = "";
+		if (idx < static_cast<int>(cmds.size()))
+			pair = cmds[idx++];
+		cmdPairVector.push_back(std::make_pair(str, pair));
 	}
 
-	// 채널에 참가
-	if (cmds.size() == 2)
-		Channel::joinChannel(client, cmds[1], "");
-	else
-		Channel::joinChannel(client, cmds[1], cmds[2]);
+	for (int i = 0; i < static_cast<int>(cmdPairVector.size()); i++)
+	{
+		std::string room = cmdPairVector[i].first;
+		std::string param = cmdPairVector[i].second;
+
+		// 잘못된 채널 이름인 경우
+		if (Channel::isInvalidChannelName(room))
+		{
+			client.sendMsg(ServerMsg::INVALIDCHANNEL(client.getNick(), room));
+			continue;
+		}
+		// 이미 참여한 채널인 경우
+		if (client.isClientMemberOfChannel(room))
+		{
+			continue;
+		}
+
+		// 채널에 참가
+		Channel::joinChannel(client, room, param);
+	}
 }
