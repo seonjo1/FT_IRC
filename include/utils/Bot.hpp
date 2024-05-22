81e510bdc8356c0ecd4bdca206a588d7a35a5a16
seonjo1 <tjdgus9684@gmail.com>
2024-05-22 17:36:44 +0900

    Added Bot : Bot 파일 추가

---
+++ include/utils/Bot.hpp
@@ -0,0 +1,16 @@
+#ifndef BOT_HPP
+# define BOT_HPP
+
+class Bot
+{
+public:
+	Bot();
+	Bot(const Bot& copy);
+	~Bot();
+	Bot&	operator=(const Bot& copy);
+
+private:
+
+};
+
+#endif
---
+++ src/utils/Bot.cpp
@@ -0,0 +1,17 @@
+#include "Bot.hpp"
+
+Bot::Bot()
+{
+}
+
+Bot::Bot(const Bot& copy)
+{
+}
+
+Bot::~Bot()
+{
+}
+
+Bot& Bot::operator=(const Bot& copy)
+{
+}
